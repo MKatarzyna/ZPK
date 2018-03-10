8c585fbfa5787cfc8dd94076fe260ea4f7a13864
MKatarzyna <katarzyna.mural@gmail.com>
2018-03-10 16:08:25 +0100

    dodano strzalki w swiatlach bezkolizyjnych, przyciski sterujace sygnalizacja, miejsce na sekundnik

--- Sygnalizator/Sygnalizator/Form1.cs
+++ Sygnalizator/Sygnalizator/Form1.cs
@@ -25,5 +25,39 @@ namespace Sygnalizator
             trafficLightsComponent3.StartCounter();
             trafficLightsComponent4.StartCounter();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            trafficLightsComponent1.NonCollisionMode = true;
+            trafficLightsComponent2.NonCollisionMode = true;
+            trafficLightsComponent3.NonCollisionMode = true;
+            trafficLightsComponent4.NonCollisionMode = true;
+
+            trafficLightsComponent4.QueueNumber = 1;
+            trafficLightsComponent3.QueueNumber = 2;
+            trafficLightsComponent2.QueueNumber = 3;
+            trafficLightsComponent1.QueueNumber = 4;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            trafficLightsComponent1.NonCollisionMode = false;
+            trafficLightsComponent2.NonCollisionMode = false;
+            trafficLightsComponent3.NonCollisionMode = false;
+            trafficLightsComponent4.NonCollisionMode = false;
+
+            trafficLightsComponent4.QueueNumber = 1;
+            trafficLightsComponent3.QueueNumber = 2;
+            trafficLightsComponent2.QueueNumber = 1;
+            trafficLightsComponent1.QueueNumber = 2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            trafficLightsComponent1.StopCounter();
+            trafficLightsComponent2.StopCounter();
+            trafficLightsComponent3.StopCounter();
+            trafficLightsComponent4.StopCounter();
+        }
     }
 }
--- Sygnalizator/TrafficLightsComponent/Class1.cs
+++ Sygnalizator/TrafficLightsComponent/Class1.cs
@@ -30,6 +30,10 @@ namespace TrafficLightsComponent
         int heightOfCircle;
         int freeSpaceBar;
 
+        int height1of4;
+        int height2of4;
+        int height3of4;
+
         Color brighterGreenColor = Color.FromArgb(255, 0, 255, 0);
         Color darkerGreenColor = Color.FromArgb(255, 0, 102, 0);
         Color brighterYellowColor = Color.FromArgb(255, 255, 255, 0);
@@ -44,8 +48,12 @@ namespace TrafficLightsComponent
         SolidBrush brighterRedBrush;
         SolidBrush darkerRedBrush;
         SolidBrush backgroundOfComponent;
+        SolidBrush backgroundOfCounter;
 
-        Pen penLine = new Pen(Color.FromArgb(255, 0, 255, 0), 6); // Color.Black
+        Pen penLineBrightGreen = new Pen(Color.FromArgb(255, 0, 255, 0), 6); // brightGreen
+        Pen penLineBrightYellow = new Pen(Color.FromArgb(255, 255, 255, 0), 6); // brightYellow
+        Pen penLineBrightRed = new Pen(Color.FromArgb(255, 255, 0, 0), 6); // brightRed
+        Pen penLineBlack = new Pen(Color.Black, 6); // Color Black
 
         // dodanie do panelu przełącznika trybu standard i niekolizyjnego
         [Category("Layout")]
@@ -137,73 +145,136 @@ namespace TrafficLightsComponent
             timeForGreenLight = 5;
             autoTime = 1; // czas na żółte światła
             sizeOfComponent = 5;
-            Size = new Size(sizeOfComponent * 20, sizeOfComponent * 30); // ustawianie rozmiaru     
+            Size = new Size(sizeOfComponent * 20, sizeOfComponent * 40); // ustawianie rozmiaru  
+            height1of4 = (int)(Size.Height / 4); // 1/4 komponentu przeznaczona na licznik
+            height2of4 = (int)(Size.Height * 2 / 4);
+            height3of4 = (int)(Size.Height * 3 / 4);
             chosenDirection = 0;
         }
 
         // namalowanie niezapalonych świateł
         private void defaultStateOfLights(PaintEventArgs e, Graphics g)
         {
-            g.FillEllipse(darkerRedBrush, freeSpaceBar, freeSpaceBar, widthOfCircle, heightOfCircle);
-            g.FillEllipse(darkerYellowBrush, freeSpaceBar, ((int)(Size.Height * 1/3)) + freeSpaceBar, widthOfCircle, heightOfCircle);
-            g.FillEllipse(darkerGreenBrush, freeSpaceBar, ((int)(Size.Height * 2/3)) + freeSpaceBar, widthOfCircle, heightOfCircle);
+            g.FillEllipse(darkerRedBrush, freeSpaceBar, freeSpaceBar + height1of4, widthOfCircle, heightOfCircle);
+            g.FillEllipse(darkerYellowBrush, freeSpaceBar, height2of4 + freeSpaceBar, widthOfCircle, heightOfCircle);
+            g.FillEllipse(darkerGreenBrush, freeSpaceBar, height3of4 + freeSpaceBar, widthOfCircle, heightOfCircle);
         }
 
         // namalowanie wlączongo czerwonego swiatla
         private void turnOnRedLight(PaintEventArgs e, Graphics g)
         {
-            g.FillEllipse(brighterRedBrush, freeSpaceBar, freeSpaceBar, widthOfCircle, heightOfCircle);
+            g.FillEllipse(brighterRedBrush, freeSpaceBar, freeSpaceBar + height1of4, widthOfCircle, heightOfCircle);
+        }
+
+        // namalowanie wlączongo czerwonego swiatla bezkolizyjnego ze strzalkami
+        private void turnOnRedLightNonCollision(PaintEventArgs e, Graphics g)
+        {
+            penLineBlack.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor; // jaki rodzaj linii -> strzałka
+            penLineBrightRed.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+
+            // wygaszona zielona strzałka
+            g.DrawLine(penLineBlack, freeSpaceBar, height3of4 + freeSpaceBar + heightOfCircle / 2, freeSpaceBar + widthOfCircle / 2, height3of4 + freeSpaceBar + heightOfCircle / 2); // strzałka w lewo
+            g.DrawLine(penLineBlack, freeSpaceBar + widthOfCircle / 2, height3of4 + freeSpaceBar, freeSpaceBar + widthOfCircle / 2, height3of4 + freeSpaceBar + heightOfCircle);    // strzałka prosto
+            // wygaszona strzalka zolta
+            g.DrawLine(penLineBlack, freeSpaceBar, height2of4 + freeSpaceBar + heightOfCircle / 2, freeSpaceBar + widthOfCircle / 2, height2of4 + freeSpaceBar + heightOfCircle / 2); // strzałka w lewo
+            g.DrawLine(penLineBlack, freeSpaceBar + widthOfCircle / 2, height2of4 + freeSpaceBar, freeSpaceBar + widthOfCircle / 2, height2of4 + freeSpaceBar + heightOfCircle);    // strzałka prosto
+            // zapalona strzalka czerowna
+            g.DrawLine(penLineBrightRed, freeSpaceBar, freeSpaceBar + heightOfCircle / 2 + height1of4, freeSpaceBar + widthOfCircle / 2, freeSpaceBar + heightOfCircle / 2 + height1of4); // strzałka w lewo
+            g.DrawLine(penLineBrightRed, freeSpaceBar + widthOfCircle / 2, freeSpaceBar + height1of4, freeSpaceBar + widthOfCircle / 2, freeSpaceBar + heightOfCircle + height1of4);    // strzałka prosto
         }
 
         // namalowanie wlączongo żółteg, czerwonego swiatla
         private void turnOnRedYellowLights(PaintEventArgs e, Graphics g)
         {
-            g.FillEllipse(brighterRedBrush, freeSpaceBar, freeSpaceBar, widthOfCircle, heightOfCircle);
-            g.FillEllipse(brighterYellowBrush, freeSpaceBar, ((int)(Size.Height * 1/3)) + freeSpaceBar, widthOfCircle, heightOfCircle);
+            g.FillEllipse(brighterRedBrush, freeSpaceBar, freeSpaceBar + height1of4, widthOfCircle, heightOfCircle);
+            g.FillEllipse(brighterYellowBrush, freeSpaceBar, height2of4 + freeSpaceBar, widthOfCircle, heightOfCircle);
+        }
+
+        // namalowanie wlączongo żółteg, czerwonego swiatla bezkolizyjnego ze strzalkami
+        private void turnOnRedYellowLightsNonCollision(PaintEventArgs e, Graphics g)
+        {
+            penLineBlack.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor; // jaki rodzaj linii -> strzałka
+            penLineBrightRed.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+            penLineBrightYellow.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+
+            // wygaszona zielona strzałka
+            g.DrawLine(penLineBlack, freeSpaceBar, height3of4 + freeSpaceBar + heightOfCircle / 2, freeSpaceBar + widthOfCircle / 2, height3of4 + freeSpaceBar + heightOfCircle / 2); // strzałka w lewo
+            g.DrawLine(penLineBlack, freeSpaceBar + widthOfCircle / 2, height3of4 + freeSpaceBar, freeSpaceBar + widthOfCircle / 2, height3of4 + freeSpaceBar + heightOfCircle);    // strzałka prosto
+            // wygaszona strzalka zolta
+            g.DrawLine(penLineBrightYellow, freeSpaceBar, height2of4 + freeSpaceBar + heightOfCircle / 2, freeSpaceBar + widthOfCircle / 2, height2of4 + freeSpaceBar + heightOfCircle / 2); // strzałka w lewo
+            g.DrawLine(penLineBrightYellow, freeSpaceBar + widthOfCircle / 2, height2of4 + freeSpaceBar, freeSpaceBar + widthOfCircle / 2, height2of4 + freeSpaceBar + heightOfCircle);    // strzałka prosto
+            // zapalona strzalka czerowna
+            g.DrawLine(penLineBrightRed, freeSpaceBar, freeSpaceBar + heightOfCircle / 2 + height1of4, freeSpaceBar + widthOfCircle / 2, freeSpaceBar + heightOfCircle / 2 + height1of4); // strzałka w lewo
+            g.DrawLine(penLineBrightRed, freeSpaceBar + widthOfCircle / 2, freeSpaceBar + height1of4, freeSpaceBar + widthOfCircle / 2, freeSpaceBar + heightOfCircle + height1of4);    // strzałka prosto
         }
 
         // namalowanie wlączongo zielonego swiatla
         private void turnOnGreenLight(PaintEventArgs e, Graphics g)
         {
-            g.FillEllipse(brighterGreenBrush, freeSpaceBar, ((int)(Size.Height * 2/3)) + freeSpaceBar, widthOfCircle, heightOfCircle);
+            g.FillEllipse(brighterGreenBrush, freeSpaceBar, height3of4 + freeSpaceBar, widthOfCircle, heightOfCircle);
         }
 
         // namalowanie wlączongo zoltego swiatla
         private void turnOnYellowLight(PaintEventArgs e, Graphics g)
         {
-            g.FillEllipse(brighterYellowBrush, freeSpaceBar, ((int)(Size.Height * 1/3)) + freeSpaceBar, widthOfCircle, heightOfCircle);
+            g.FillEllipse(brighterYellowBrush, freeSpaceBar, height2of4 + freeSpaceBar, widthOfCircle, heightOfCircle);
+        }
+
+        // namalowanie wlączongo zoltego swiatla bezkolizyjnego ze strzalkami
+        private void turnOnYellowLightNonCollision(PaintEventArgs e, Graphics g)
+        {
+            penLineBlack.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor; // jaki rodzaj linii -> strzałka
+            penLineBrightYellow.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor;
+
+            // wygaszona zielona strzałka
+            g.DrawLine(penLineBlack, freeSpaceBar, height3of4 + freeSpaceBar + heightOfCircle / 2, freeSpaceBar + widthOfCircle / 2, height3of4 + freeSpaceBar + heightOfCircle / 2); // strzałka w lewo
+            g.DrawLine(penLineBlack, freeSpaceBar + widthOfCircle / 2, height3of4 + freeSpaceBar, freeSpaceBar + widthOfCircle / 2, height3of4 + freeSpaceBar + heightOfCircle);    // strzałka prosto
+            // wygaszona strzalka zolta
+            g.DrawLine(penLineBrightYellow, freeSpaceBar, height2of4 + freeSpaceBar + heightOfCircle / 2, freeSpaceBar + widthOfCircle / 2, height2of4 + freeSpaceBar + heightOfCircle / 2); // strzałka w lewo
+            g.DrawLine(penLineBrightYellow, freeSpaceBar + widthOfCircle / 2, height2of4 + freeSpaceBar, freeSpaceBar + widthOfCircle / 2, height2of4 + freeSpaceBar + heightOfCircle);    // strzałka prosto
+            // zapalona strzalka czerowna
+            g.DrawLine(penLineBlack, freeSpaceBar, freeSpaceBar + heightOfCircle / 2 + height1of4, freeSpaceBar + widthOfCircle / 2, freeSpaceBar + heightOfCircle / 2 + height1of4); // strzałka w lewo
+            g.DrawLine(penLineBlack, freeSpaceBar + widthOfCircle / 2, freeSpaceBar + height1of4, freeSpaceBar + widthOfCircle / 2, freeSpaceBar + heightOfCircle + height1of4);    // strzałka prosto
         }
 
         // namalowanie włączonego strzalki i swiatla kierunkowego w prawo
         private void turnOnConditionalRightLight(PaintEventArgs e, Graphics g)
         {
-            g.FillEllipse(darkerGreenBrush, freeSpaceBar + Size.Width / 2, ((int)(Size.Height * 2 / 3)) + freeSpaceBar, widthOfCircle, heightOfCircle);
-            penLine.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor; // jaki rodzaj linii -> strzałka
-            g.DrawLine(penLine, freeSpaceBar + widthOfCircle + (int)(Size.Width / 2), ((int)(Size.Height * 2 / 3)) + freeSpaceBar + heightOfCircle / 2, freeSpaceBar + (int)(Size.Width / 2), ((int)(Size.Height * 2 / 3)) + freeSpaceBar + heightOfCircle / 2); // strzałka warunkowa w prawo
+            g.FillEllipse(darkerGreenBrush, freeSpaceBar + Size.Width / 2, height3of4 + freeSpaceBar, widthOfCircle, heightOfCircle);
+            penLineBrightGreen.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor; // jaki rodzaj linii -> strzałka
+            g.DrawLine(penLineBrightGreen, freeSpaceBar + widthOfCircle + (int)(Size.Width / 2), height3of4 + freeSpaceBar + heightOfCircle / 2, freeSpaceBar + (int)(Size.Width / 2), height3of4 + freeSpaceBar + heightOfCircle / 2); // strzałka warunkowa w prawo
         }
 
         // namalowanie wylaczonego swiatla kierunkowego w prawo
         private void turnOffConditionalRightLight(PaintEventArgs e, Graphics g)
         {
-            g.FillEllipse(darkerGreenBrush, freeSpaceBar + Size.Width / 2, ((int)(Size.Height * 2 / 3)) + freeSpaceBar, widthOfCircle, heightOfCircle);
+            g.FillEllipse(darkerGreenBrush, freeSpaceBar + Size.Width / 2, height3of4 + freeSpaceBar, widthOfCircle, heightOfCircle);
 
         }
 
         // namalowanie wlączonego swiatla bezkolizyjnego w lewo
         private void turnOnNonCollisionLeftLight(PaintEventArgs e, Graphics g)
         {
-            g.FillEllipse(darkerGreenBrush, freeSpaceBar, ((int)(Size.Height * 2/3)) + freeSpaceBar, widthOfCircle, heightOfCircle);
-            penLine.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor; // jaki rodzaj linii -> strzałka
+            g.FillEllipse(darkerGreenBrush, freeSpaceBar, height3of4 + freeSpaceBar, widthOfCircle, heightOfCircle);
+            penLineBrightGreen.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor; // jaki rodzaj linii -> strzałka
+            penLineBlack.StartCap = System.Drawing.Drawing2D.LineCap.ArrowAnchor; // jaki rodzaj linii -> strzałka
 
-            g.DrawLine(penLine, freeSpaceBar, ((int)(Size.Height * 2/3)) + freeSpaceBar + heightOfCircle / 2, freeSpaceBar + widthOfCircle / 2, ((int)(Size.Height * 2/3)) + freeSpaceBar + heightOfCircle / 2); // strzałka w lewo
-            g.DrawLine(penLine, freeSpaceBar + widthOfCircle / 2, ((int)(Size.Height * 2/3)) + freeSpaceBar, freeSpaceBar + widthOfCircle / 2, ((int)(Size.Height * 2/3)) + freeSpaceBar + heightOfCircle);    // strzałka prosto
+            // zapalone zielone strzałki
+            g.DrawLine(penLineBrightGreen, freeSpaceBar, height3of4 + freeSpaceBar + heightOfCircle / 2, freeSpaceBar + widthOfCircle / 2, height3of4 + freeSpaceBar + heightOfCircle / 2); // strzałka w lewo
+            g.DrawLine(penLineBrightGreen, freeSpaceBar + widthOfCircle / 2, height3of4 + freeSpaceBar, freeSpaceBar + widthOfCircle / 2, height3of4 + freeSpaceBar + heightOfCircle);    // strzałka prosto
+            // wygaszona strzalka zolta
+            g.DrawLine(penLineBlack, freeSpaceBar, height2of4 + freeSpaceBar + heightOfCircle / 2, freeSpaceBar + widthOfCircle / 2, height2of4 + freeSpaceBar + heightOfCircle / 2); // strzałka w lewo
+            g.DrawLine(penLineBlack, freeSpaceBar + widthOfCircle / 2, height2of4 + freeSpaceBar, freeSpaceBar + widthOfCircle / 2, height2of4 + freeSpaceBar + heightOfCircle);    // strzałka prosto
+            // wygaszona strzalka czerowna
+            g.DrawLine(penLineBlack, freeSpaceBar, freeSpaceBar + heightOfCircle / 2 + height1of4, freeSpaceBar + widthOfCircle / 2, freeSpaceBar + heightOfCircle / 2 + height1of4); // strzałka w lewo
+            g.DrawLine(penLineBlack, freeSpaceBar + widthOfCircle / 2, freeSpaceBar + height1of4, freeSpaceBar + widthOfCircle / 2, freeSpaceBar + heightOfCircle + height1of4);    // strzałka prosto
         }
 
         // cykliczne odmalowanie swiatel i namalowanie pierwszy raz
         protected override void OnPaint(PaintEventArgs e)
         {
             Width = sizeOfComponent * 20;
-            Height = sizeOfComponent * 30;
+            Height = sizeOfComponent * 40;
             Graphics g = e.Graphics;
             widthOfCircle = (int)((Size.Width  / 2) * 0.8);
             heightOfCircle = widthOfCircle;
@@ -216,15 +287,17 @@ namespace TrafficLightsComponent
             brighterRedBrush = new SolidBrush(brighterRedColor);
             darkerRedBrush = new SolidBrush(darkerRedColor);
             backgroundOfComponent = new SolidBrush(Color.FromArgb(255, 64, 64, 64));
-            
+            backgroundOfCounter = new SolidBrush(Color.FromArgb(255, 90, 90, 90));
+
             // glowny czarny prostokat komponentu
             g.FillRectangle(backgroundOfComponent, 0, 0, (int)(Size.Width / 2), (int)Size.Height);
-
+            g.FillRectangle(backgroundOfCounter, freeSpaceBar, freeSpaceBar, (int)(Size.Width / 2 - freeSpaceBar * 2), (int)(height1of4 - freeSpaceBar * 2));
+            
             if (nonCollisionMode == false)
             {
                 // wyrysowanie bocznego kwadratu komponentu do strzałki warunkowej
-                g.FillRectangle(backgroundOfComponent, (int)(Size.Width / 2), (int)((Size.Height / 3) * 2), (int)(Size.Width / 2), (int)(Size.Height / 3));
-                g.FillEllipse(darkerGreenBrush, freeSpaceBar + Size.Width / 2, ((int)(Size.Height * 2 / 3)) + freeSpaceBar, widthOfCircle, heightOfCircle);
+                g.FillRectangle(backgroundOfComponent, (int)(Size.Width / 2), height3of4, (int)(Size.Width / 2), height1of4);
+                g.FillEllipse(darkerGreenBrush, freeSpaceBar + Size.Width / 2, height3of4 + freeSpaceBar, widthOfCircle, heightOfCircle);
             }
 
            
@@ -254,11 +327,22 @@ namespace TrafficLightsComponent
             {
                 turnOnConditionalRightLight(e, g);
             }
-            else if (chosenDirection == 6)
+            else if (chosenDirection == 60)
             {
                 turnOnNonCollisionLeftLight(e, g);
             }
-
+            else if (chosenDirection == 10)
+            {
+                turnOnRedLightNonCollision(e, g);
+            }
+            else if (chosenDirection == 20)
+            {
+                turnOnRedYellowLightsNonCollision(e, g);
+            }
+            else if (chosenDirection == 40)
+            {
+                turnOnYellowLightNonCollision(e, g);
+            }
         }
 
         //liczniki ustalenia czasu (dlugosci cyklu) dla trybu standard i non-collision
@@ -279,6 +363,12 @@ namespace TrafficLightsComponent
             timer.Tick += new EventHandler(timerTick);
             timer.Interval = 1000; // 1 sekunda
             timer.Start();
+        }
+
+        //zatrzymanie sygnalizacji swietlnej poprzez zatrzymanie timera
+        public void StopCounter()
+        {
+            timer.Stop();
         }
 
         // odliczanie co jedna sekunde, ze sprawdzeniem jaki tryb i nastepnie włączenie świateł dla odpowiedniego komponentu
@@ -364,23 +454,23 @@ namespace TrafficLightsComponent
         {
             if (counter > (timeForAllStates * queueNumber - timeForRedLight))
             {
-                chosenDirection = 1; // RED
+                chosenDirection = 10; // RED
             }
             else if (counter > (timeForAllStates * queueNumber - timeForRedLight - autoTime))
             {
-                chosenDirection = 2; // RED YELLOW
+                chosenDirection = 20; // RED YELLOW
             }
             else if (counter > (timeForAllStates * queueNumber - timeForRedLight - autoTime - timeForGreenLight))
             {
-                chosenDirection = 6; // GREEN
+                chosenDirection = 60; // GREEN
             }
             else if (counter > (timeForAllStates * queueNumber - timeForRedLight - autoTime - timeForGreenLight - autoTime))
             {
-                chosenDirection = 4; // YELLOW
+                chosenDirection = 40; // YELLOW
             }
             else if (counter < (timeForAllStates * queueNumber - timeForRedLight - autoTime - timeForGreenLight - autoTime))
             {
-                chosenDirection = 1; // RED
+                chosenDirection = 10; // RED
             }
             Invalidate();
         }
